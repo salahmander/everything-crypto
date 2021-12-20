e18e6a936048d0703245fd4844e577f72fbca2e4
salahmander <salahabdo@hotmail.co.uk>
2021-12-20 14:56:09 +0000

    Add strict mode and ant design css to the index file.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
+import "antd/dist/antd.css";
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );

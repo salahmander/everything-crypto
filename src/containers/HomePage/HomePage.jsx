aff1d88d8d95913f403c23fa72ae2d2b29ae789e
salahmander <salahabdo@hotmail.co.uk>
2021-12-22 15:10:55 +0000

    Setup up home page structure, with dummy data ready to be insrted with live data.

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+// Destructure title from typography
+const { Title } = Typography;
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Title level={2} className="heading">
+        Global Crypto stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+      </Row>
+    </>
   );
 };
 

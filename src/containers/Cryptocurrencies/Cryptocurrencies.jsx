0616470a1f119df91ca52a3de0516b2f55f83309
salahmander <salahabdo@hotmail.co.uk>
2021-12-23 13:00:05 +0000

    Add search crypto input to filter the crypto currencies.

--- src/containers/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/containers/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
@@ -8,10 +8,26 @@ import { useGetCryptosQuery } from "../../services/cryptoAPi";
 const CryptoCurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // Filter out only the searched coin in the array of cryptocurrencies
+    const filteredData = cryptoList?.data.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptoList, searchTerm]);
 
   return (
     <>
+      <div className="search-crypto">
+        <Input
+          placeholder="Search Cryptocurrency"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>

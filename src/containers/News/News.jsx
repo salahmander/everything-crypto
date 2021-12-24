55cb81a9985df0b1b9013d410b19d5e291cb726c
salahmander <salahabdo@hotmail.co.uk>
2021-12-24 11:35:16 +0000

    Implement filter selector to select news related to a specific crypto.

--- src/containers/News/News.jsx
+++ src/containers/News/News.jsx
@@ -21,6 +21,25 @@ const News = ({ simplified }) => {
   if (!cryptoNews?.value) return "Loading...";
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((currency) => (
+              <Option value={currency.name}>{currency.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, index) => (
         <Col xs={24} sm={12} lg={8} key={index}>
           <Card hoverable className="news-card">

f4473e37f15b3edc2753d7d961d052637c5ae08a
salahmander <salahabdo@hotmail.co.uk>
2021-12-31 23:52:36 +0000

    Add a temp loading when data is being fetched.

--- src/containers/CryptoDetails/CryptoDetails.jsx
+++ src/containers/CryptoDetails/CryptoDetails.jsx
@@ -33,6 +33,8 @@ const CryptoDetails = () => {
 
   const cryptoDetails = data?.data?.coin;
 
+  if(isFetching) return "Loading..."
+
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
   const stats = [
